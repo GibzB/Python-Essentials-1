4fd5da00f18fdfe2c836356a1614a1131c1bb4fa
Billy Gibendi <ralphgibendi01@gmail.com>
2023-01-25 15:26:31 +0300

    finished module 5

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -200,13 +200,15 @@ print("c =", c)
 # Scenario
 #Here is a short story:
 
-#Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. They were all very happy and lived for a long time. End of story.
+# Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. 
+# They were all very happy and lived for a long time. End of story.
 
 #Your task is to:
 
 #1. create the variables: john, mary, and adam;
 #2. assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
-#3. having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+#3. having stored the numbers in the variables, print the variables on one line, 
+#   and separate each of them with a comma;
 #4. now create a new variable named total_apples equal to the addition of the three previous variables.
 #5. print the value stored in total_apples to the console;
 #6. experiment with your code: create new variables, assign different values to them, and perform various arithmetic operations on them (e.g., +, -, *, /, //, etc.). Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
@@ -220,5 +222,103 @@ print(john_apples,mary_apples,adam_apples, sep=',')
 print(total_apples)
 print('Total number of apples:', total_apples)
 
-#
+# Shortcut Operators
+x = x * 2
+sheep = sheep + 1
 
+# Python offers you a shortened way of writing operations like these:
+x *= 2
+sheep += 1
+
+#  LAB   Variables ‒ a simple converter
+
+# Miles and kilometers are units of length or distance.
+# Bearing in mind that 1 mile is equal to approximately 1.61 kilometers, complete the program in the editor so that it converts:
+
+#miles to kilometers;
+#kilometers to miles.
+#Do not change anything in the existing code. Write your code in the places indicated by ###. Test your program with the data we've provided in the source code.
+
+#Pay particular attention to what is going on inside the print() function. 
+# Analyze how we provide multiple arguments to the function, and how we output the expected data.
+
+#Note that some of the arguments inside the print() function are strings (e.g., "miles is", whereas some other are variables (e.g., miles).
+
+# Expected output
+# 7.38 miles is 11.88 kilometers
+# 12.25 kilometers is 7.61 miles
+
+# Solution:
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61  
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+#  LAB   Operators and expressions
+
+# Scenario
+#Take a look at the code in the editor: it reads a float value, puts it into a variable named x, and prints the value of a variable named y. 
+# Your task is to complete the code in order to evaluate the following expression:
+
+#    3x3 - 2x2 + 3x - 1
+
+# Note how we change data type to make sure that x is of type float.
+# The result should be assigned to y
+
+#  x =  # Hardcode your test data here.
+#  x = float(x)
+#   Write your code here.
+#  print("y =", y)
+
+# Solution:
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+
+# LAB   Comments
+# Scenario
+# The code in the editor contains comments. Try to improve it: 
+# add or remove comments where you find it appropriate (yes, sometimes removing a comment can make the code more readable), 
+# and change variable names where you think this will improve code comprehension.
+
+# this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the number of seconds in 3 hour
+
+# Solution:
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+b = 3 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+print('Goodbye')#here we should also print "Goodbye", but a programmer didn't have time to write any code
+print('Seconds in 3 Hours: ', b * seconds) #this is the end of the program that computes the number of seconds in 3 hour
