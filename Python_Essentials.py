3f9b0e68ac4a7946b0683d5139a24d752904251b
Billy Gibendi <ralphgibendi01@gmail.com>
2023-01-31 22:38:30 +0300

    While loops

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -658,9 +658,85 @@ while number != -1:
 # Print the largest number.
 print("The largest number is:", largest_number)
 
+# LAB   Essentials of the if-else statement
+# Scenario
+# Once upon a time there was a land – a land of milk and honey, inhabited by happy and prosperous people. The people paid taxes, of course – their happiness had limits. The most important tax, called the Personal Income Tax (PIT for short) had to be paid once a year, and was evaluated using the following rule:
+
+# if the citizen's income was not higher than 85,528 thalers, the tax was equal to 18% of the income minus 556 thalers and 2 cents (this was what they called tax relief)
+# if the income was higher than this amount, the tax was equal to 14,839 thalers and 2 cents, plus 32% of the surplus over 85,528 thalers.
+# Your task is to write a tax calculator.
+
+# It should accept one floating-point value: the income.
+# Next, it should print the calculated tax, rounded to full thalers. There's a function named round() which will do the rounding for you – you'll find it in the skeleton code in the editor.
+# Note: this happy country never returned any money to its citizens. If the calculated tax was less than zero, it would only mean no tax at all (the tax was equal to zero). Take this into consideration during your calculations.
+
+# Look at the code in the editor – it only reads one input value and outputs a result, so you need to complete it with some smart calculations.
+
+# Test your code using the data we've provided.
 
 
-# The while loop
+# Test Data: Sample input: 10000  Expected output:The tax is: 1244.0 thalers
+#            Sample input: 100000 Expected output:The tax is: 19470.0 thalers
+#            Sample input: 1000   Expected output:The tax is: 0.0 thalers
+#            Sample input: -100   Expected output:The tax is: 0.0 thalers
+
+
+# SOLUTION:
+income = float(input("What's the annual income? : "))
+
+if income < 85528:
+	taxes = income * 0.18 - 556.02
+else:
+	taxes = (income - 85528) * 0.32 + 14839.02
+
+if taxes < 0.0:
+	taxes = 0.0
+
+taxes = round(taxes, 0)
+print("Tax to be paid is:", taxes, "thalers")
+ 
+# LAB   Essentials of the if-elif-else statement
+# Scenario
+# As you surely know, due to some astronomical reasons, years may be leap or common. The former are 366 days long, while the latter are 365 days long.
+
+# Since the introduction of the Gregorian calendar (in 1582), the following rule is used to determine the kind of year:
+
+# if the year number isn't divisible by four, it's a common year;
+#  otherwise, if the year number isn't divisible by 100, it's a leap year;
+#  otherwise, if the year number isn't divisible by 400, it's a common year;
+#  otherwise, it's a leap year.
+# Look at the code in the editor – it only reads a year number, and needs to be completed with the instructions implementing the test we've just described.
+
+# The code should output one of two possible messages, which are Leap year or Common year, depending on the value entered.
+
+# It would be good to verify if the entered year falls into the Gregorian era, and output a warning otherwise: Not within the Gregorian calendar period. Tip: use the != and % operators.
+
+# Test your code using the data we've provided.
+
+
+# Test Data: Sample input: 2000   Expected output: Leap year
+#            Sample input: 2015   Expected output: Common year
+#            Sample input: 1999   Expected output: Common year  
+#            Sample input: 1996   Expected output: Leap year
+#            Sample input: 1580   Expected output: Not within the Gregorian calendar period
+
+# SOLUTION:
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+else:
+    #  Write the if-elif-elif-else block here.
+    if year % 4 != 0:
+        print("Its a common year") # if the year number isn't divisible by four, it's a common year;
+    elif year % 100 != 0:
+        print("Its a leap year") # if the year number isn't divisible by 100, it's a leap year;
+    elif year % 400 !=0: 
+        print("Its a common year") # otherwise, if the year number isn't divisible by 400, it's a common year;
+    else:
+        print("It's a leap year!") # it's a leap year.
+
+# The WHILE loop
 
 # A program that reads a sequence of numbers
 # and counts how many numbers are even and how many are odd.
@@ -689,6 +765,26 @@ print("Odd numbers count:", odd_numbers)
 print("Even numbers count:", even_numbers)
 
 
+# While loop Game: Guess the secret number
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+guessed_number = int(input("Guess the secret number: "))
+
+while guessed_number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    guessed_number = int(input("Enter the number again: "))
+print(secret_number, "Well done, muggle! You are free now.")
 
 
 
