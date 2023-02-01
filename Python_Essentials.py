51eadbbd3a4744c752f50ba33183d391e01dd68c
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-01 13:45:02 +0300

    break and continue statements

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -824,3 +824,59 @@ starting_number = 1
 for exponent in range(16):
     print("2 raised to the power of", exponent, "is", starting_number)
     starting_number *= 2
+
+# break and continue statements
+
+# break - example
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# continue - example
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# More Examples 
+# break variant
+largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end the program: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+
+if counter != 0:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
+
+# continue variant
+largest_number = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Enter a number or type -1 to end the program: "))
+
+if counter:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
