f979dcc2d25dc5669e4455fa2d431905242520dc
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-08 21:12:58 +0300

    Sorting

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -1007,3 +1007,47 @@ f = x << 2
 
 print(a, b, c, d, e, f)
 
+# Sorting
+
+# Making the value of the fifth element to be copied to the second element 
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers) # Printing original list contents.
+
+numbers[0] = 111
+print("\nPrevious list contents:", numbers) # Printing previous list contents.
+
+numbers[1] = numbers[4] # Copying value of the fifth element to the second.
+print("New list contents:", numbers) # Printing current list contents.
+
+
+# More: 
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers)  # Printing original list contents.
+
+numbers[0] = 111
+print("\nPrevious list contents:", numbers)  # Printing previous list contents.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list contents:", numbers)  # Printing previous list contents.
+
+# This is the len() function
+print("\nList length:", len(numbers))  # Printing the list's length.
+
+
+# Delete instruction
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+numbers[0] = 111
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list content:", numbers)  # Printing previous list content.
+print("\nList's length:", len(numbers))  # Printing previous list length.
+
+###
+
+del numbers[1]  # Removing the second element from the list.
+print("New list's length:", len(numbers))  # Printing new list length.
+print("\nNew list content:", numbers)  # Printing current list content.
+
+###
---
+++ sorting.py
@@ -0,0 +1,6 @@
+# Let's assign a new value of 111 to the first element in the list. We do it this way:
+
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers)  # Printing original list contents.
+numbers[0] = 111
+print("New list contents: ", numbers)  # Current list contents.
