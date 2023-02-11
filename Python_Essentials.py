09c8b80d2b2ac8993bc1fc12b441c44d923f70d9
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-11 18:54:08 +0300

    LAB Solution

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -1176,3 +1176,30 @@ print(my_list)
 #        Stu Sutcliffe, and Pete Best;
 #step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
 #step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+
+# SOLUTION:
+# step 1:
+Beatles = []
+print("Step 1:", Beatles)
+
+# step 2:
+
+Beatles.append("John Lennon")
+Beatles.append("Paul McCartney")
+Beatles.append("George Harrison")
+print("Step 2:", Beatles)
+
+# step 3:
+for members in range(2):
+    Beatles.append(input("New band member: "))
+print("Step 3:", Beatles)
+
+# step 4:
+del Beatles[-1]
+del Beatles[-1]
+print("Step 4:", Beatles)
+
+# step 5:
+Beatles.insert(0, "RingoStarr")
+print("Step 5:", Beatles)
+print("The Fab:",len(Beatles))
