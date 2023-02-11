f21344db08d328d351c6102376e94ebde4e0692b
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-11 19:15:20 +0300

    sorting a list

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -1203,3 +1203,12 @@ print("Step 4:", Beatles)
 Beatles.insert(0, "RingoStarr")
 print("Step 5:", Beatles)
 print("The Fab:",len(Beatles))
+
+
+# Sorting a list
+my_list = [8, 10, 6, 2, 4] # list to sort
+
+for i in range(len(my_list) - 1): # we need (5 - 1) comparisons
+    if my_list[i] > my_list[i + 1]: # compare adjacent elements
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i] # If we end up here, we have to swap the elements.
+
