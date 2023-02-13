2f16e5d049ecbcd1bcc4c13760a7032cbb905e2b
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-13 13:34:41 +0300

    negative indices with slices

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -1284,3 +1284,8 @@ my_list = [10, 8, 6, 4, 2]
 new_list = my_list[1:3]
 print(new_list)
 
+# This is how negative indices work with the slice:
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:-1]
+print(new_list)
