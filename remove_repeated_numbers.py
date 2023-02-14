2fd88b637d93fef40d05e11ec3c46a8342687bd3
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-14 23:37:33 +0300

    Lab scenario

---
+++ remove_repeated_numbers.py
@@ -0,0 +1,8 @@
+my_list = [1, 3, 2, 4, 1, 3, 2, 6, 6, 9]
+new_list = []
+for number in my_list:  # Browse all numbers from the source list.
+	if number not in new_list:  # If the number doesn't appear within the new list...
+		new_list.append(number)  # ...append it here.
+my_list = new_list[:]  # Make a copy of new_list.
+print("The list with unique elements only:")
+print(my_list)
