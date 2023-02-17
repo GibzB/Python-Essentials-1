faf62913836f9ef9cf63795959ff121cd0ff2b25
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-17 22:32:15 +0300

    monthly average noon temperature

---
+++ list_mutation.py
@@ -0,0 +1,4 @@
+
+my_list = list(range(1, 7)) 
+my_list = my_list.append(8) 
+print(my_list)
---
+++ weather_data_capture.py
@@ -0,0 +1,17 @@
+# Imagine that you're developing a piece of software for an automatic weather station. 
+# The device records the air temperature on an hourly basis and does it throughout the month. 
+# This gives you a total of 24 × 31 = 744 values. 
+
+# Deciding which data type would be adequate for this application. In this case, a float would be best
+
+
+temps = [[0.0 for h in range(24)] for d in range(31)]
+
+total = 0.0
+ 
+for day in temps:
+    total += day[11]
+ 
+average = total / 31
+ 
+print("Average temperature at noon:", average)
