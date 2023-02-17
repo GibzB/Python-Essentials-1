52a63086aab0410f6ced0e9ea5e35f5959020a71
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-17 22:33:27 +0300

    highest temperature during the whole month

--- weather_data_capture.py
+++ weather_data_capture.py
@@ -15,3 +15,20 @@ for day in temps:
 average = total / 31
  
 print("Average temperature at noon:", average)
+
+
+# highest temperature during the whole month
+temps = [[0.0 for h in range(24)] for d in range(31)]
+#
+# The matrix is magically updated here.
+#
+
+highest = -100.0
+
+for day in temps:
+    for temp in day:
+        if temp > highest:
+            highest = temp
+
+print("The highest temperature was:", highest)
+
