e11471b4cd118edb9999babf81776dae42003903
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-20 21:18:16 +0300

    shadowing explained

--- shadowing.py
+++ shadowing.py
@@ -1,3 +1,8 @@
+# Shadowing in Python occurs when a variable or function with the same name 
+# as an existing variable or function is defined within a more nested scope. 
+# 
+# This can lead to unexpected behavior as the inner variable or function will "shadow" the outer one, 
+# making it inaccessible or causing the code to behave differently than intended.
 def message(number):
     print("Enter a number:", number)
  
