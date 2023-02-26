8fc9ef60c1852ff0d41f4be0b6e7e780a6245c0c
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-26 19:26:52 +0300

    function interaction with its arguements

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -1834,3 +1834,12 @@ var = 1
 my_function()
 print(var)
 
+# how the function interacts with its arguments.
+def my_function(n):
+    print("I got", n)
+    n += 1
+    print("I have", n)
+
+var = 1
+my_function(var)
+print(var)
