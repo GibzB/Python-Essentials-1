c48f02178c15c18425f1b9096c112a8486bf5b48
Billy Gibendi <ralphgibendi01@gmail.com>
2023-02-26 12:35:26 +0300

    Trial

--- test2.py
+++ test2.py
@@ -1,9 +1,11 @@
-def strange_list_fun(n):
-    strange_list = []
-    
-    for i in range(0, n):
-        strange_list.insert(0, i)
-    
-    return strange_list
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+    print("Happy New Year!")
+ 
+# When invoked without any arguments:
 
-print(strange_list_fun(5))
+happy_new_year(False)
