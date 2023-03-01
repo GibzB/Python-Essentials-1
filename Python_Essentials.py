4fc05bacfc3ec4298ed6e3663c52be8aa18fad15
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-01 16:57:00 +0300

    Recursive implementation of the factorial function

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2056,3 +2056,13 @@ def fib(n):
 for n in range(1, 10):
     print(n, "->", fib(n))
 
+# Recursive implementation of the factorial function.
+
+def factorial(n):
+    if n == 1: # The base case (termination condition.)
+        return 1
+    else:
+        return n * factorial(n - 1)
+ 
+ 
+print(factorial(4)) # 4 * 3 * 2 * 1 = 24
