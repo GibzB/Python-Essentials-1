882083ae51e6fea63991d02fa9cc9a4b9fd20821
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-01 16:17:55 +0300

    Pythagorean theorem

--- test2.py
+++ test2.py
@@ -1,7 +1,12 @@
 def is_a_triangle(a, b, c):
-    if a + b <= c or b + c <= a or c + a <= b:
-        return False
-    return True
+    return a + b > c and b + c > a and c + a > b
  
-print(is_a_triangle(1, 1, 1))
-print(is_a_triangle(1, 1, 3))
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+ 
+print(area_of_triangle(1., 1., 2. ** .5))
