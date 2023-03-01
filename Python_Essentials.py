26527a3eea77b81ea12a498c5694a8ef6f2ddc54
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-01 16:31:29 +0300

    Fibonacci numbers

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2001,3 +2001,30 @@ def factorial_function(n):
  
 for n in range(1, 6): # testing
     print(n, factorial_function(n))
+
+# Fibonacci numbers
+# 
+fib_1 = 1 
+fib_2 = 1 
+fib_3 = 1 + 1  
+fib_4 = 1 + 2  
+fib_5 = 2 + 3  
+fib_6 = 3 + 5  
+fib_7 = 5 + 8 
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+ 
+    elem_1 = elem_2 = 1
+    the_sum = 0
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+    return the_sum
+ 
+for n in range(1, 10): # testing
+    print(n, "->", fib(n))
+
