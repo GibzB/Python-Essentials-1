fcf6dcfe92f32ca771c11d40733644becd46d764
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-03 22:26:31 +0300

    Using dictionaries

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2150,3 +2150,12 @@ empty_dictionary = {}
 print(dictionary)
 print(phone_numbers)
 print(empty_dictionary)
+
+# How to use Dictionaries
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+phone_numbers = {'boss' : 5551234567, 'Suzy' : 22657854310}
+empty_dictionary = {}
+
+# Print the values here.
+print(dictionary['cat'])
+print(phone_numbers['Suzy'])
