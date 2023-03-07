e524e3ed0c04b57e3cc021e31f45fa78d7b09e78
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-07 21:35:06 +0300

    more examples on adding dictionaries

--- dictionary_add.py
+++ dictionary_add.py
@@ -2,3 +2,13 @@ dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
  
 dictionary['swan'] = 'cygne'
 print(dictionary)
+
+
+# Example 2: Add a new item to a dictionary using the "update()" method
+phonebook = {} # an empty dictionary
+ 
+phonebook["Adam"] = 3456783958 # create/add a key-value pair
+print(phonebook) # outputs: {'Adam': 3456783958}
+ 
+del phonebook["Adam"]
+print(phonebook) # outputs: {}
