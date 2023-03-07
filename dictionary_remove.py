0eba70ee4c5966508185af4357ca3ce4b14d5a5d
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-07 21:25:02 +0300

    remove last item in dictionary

--- dictionary_remove.py
+++ dictionary_remove.py
@@ -2,3 +2,7 @@ dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
  
 del dictionary['dog']
 print(dictionary)
+
+# An alternative is to use the "popitem()" method
+dictionary.popitem()
+print(dictionary)
