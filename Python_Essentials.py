554604a2228f2b56fafef5d4606ec34933dd2615
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-07 20:15:05 +0300

    copying dictionaries

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2220,4 +2220,24 @@ for english, french in dictionary.items():
 # 17. The viewkeys() method returns a view object that displays a list of all the keys in the dictionary.
 
 # TUPLES & DICTIONARIES
+school_class = {}
+
+while True:
+    name = input("Enter the student's name: ")
+    if name == '':
+        break
+    score = int(input("Enter the student's score (0-99): "))
+    if score not in range(0, 99):
+	    break
+    if name in school_class:
+        school_class[name] += (score,)
+    else:
+        school_class[name] = (score,) 
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
 
---
+++ copying_dictionary.py
@@ -0,0 +1,9 @@
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+ 
+copy_dictionary = pol_eng_dictionary.copy()
+print(pol_eng_dictionary) 
+print(copy_dictionary) # has the same values as copy_dictionary
