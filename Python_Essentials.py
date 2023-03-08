1266c871bc70e6f84b134da80a343c0d1e79976e
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-08 22:09:49 +0300

    Used the try & except branch

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2320,3 +2320,20 @@ print(colors_dictionary)
 
 value = int(input('Enter a natural number: '))
 print('The reciprocal of', value, 'is', 1/value)
+
+# The try-except branch
+
+try:
+	# It's a place where
+	# you can do something 
+    # without asking for permission.
+except:
+	# It's a spot dedicated to 
+    # solemnly begging for forgiveness.
+
+# Solved
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except:
+    print('I do not know what to do.')
--- exceptions.py
+++ exceptions.py
@@ -3,3 +3,9 @@ print('The reciprocal of', value, 'is', 1/value)
 
 # Inputing a float returns a "ValueError: invalid literal for int()"
 
+# Solved
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except:
+    print('I do not know what to do.')
