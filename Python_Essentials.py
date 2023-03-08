5d6e073d69a36bf47f971bb017d7a1fc875dc1ba
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-08 22:18:02 +0300

    two exception after one try

--- Python_Essentials.py
+++ Python_Essentials.py
@@ -2337,3 +2337,12 @@ try:
     print('The reciprocal of', value, 'is', 1/value)        
 except:
     print('I do not know what to do.')
+
+# Two exceptions after one try
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except ValueError:
+    print('I do not know what to do.')    
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.') 
--- exceptions.py
+++ exceptions.py
@@ -9,3 +9,5 @@ try:
     print('The reciprocal of', value, 'is', 1/value)        
 except:
     print('I do not know what to do.')
+
+# 
