5fd089b23008380b048a6fdff71324cd52a67b8c
Billy G <10028885+GibzB@users.noreply.github.com>
2024-07-04 17:10:02 +0300

    Usiku List Question

---
+++ usiku.py
@@ -0,0 +1,71 @@
+# list
+
+# Create an empty list called define_my_list.
+#define_my_list = []
+#print(define_my_list)
+
+# Append the following elements to define_my_list: 10, 20, 30, 40.
+# vivian --- 
+#define_my_list.append([10, 20, 30, 40])
+#print("after append:", define_my_list)
+
+# idah --- 
+#listappend = define_my_list.append([10, 20, 30, 40])
+#print(listappend)
+
+# samuel ---
+#numbers = (10, 20, 30, 40)
+#define_my_list.append(numbers)
+#print(define_my_list.append(numbers))
+
+
+# idah 2 ---
+#define_my_list.append(10) 
+#define_my_list.append(20) 
+#efine_my_list.append(30) 
+#define_my_list.append(40)
+print(define_my_list)
+ 
+
+
+
+
+# Insert the value 7 at the second position in the list.
+#print(define_my_list[1])
+#define_my_list.insert(1,7) # adds value to the list , list gets bigger
+#print(define_my_list)
+
+#define_my_list[1] = 16 #replace value in list
+#print(define_my_list)
+
+#Extend define_my_list with another list: [50, 60, 70].
+#print(define_my_list)
+#list_2 = [50,60,70]
+#print(list_2)
+
+
+#define_my_list.extend(list_2)
+#print(define_my_list)
+
+
+
+#Remove the last element from define_my_list.
+#define_my_list.pop(0)
+
+#del define_my_list[-1] # use index
+#print(define_my_list)
+
+#Sort define_my_list in ascending order.
+#define_my_list.sort() # sorts by default in ascending order
+#define_my_list.sort(reverse = True) # sort in descending order
+#print(define_my_list)
+
+#Find and print the index of the value 30 in define_my_list.
+
+index_30 = define_my_list.index(20)
+print("Index 20 is:", index_30)
+
+
+
+
+
